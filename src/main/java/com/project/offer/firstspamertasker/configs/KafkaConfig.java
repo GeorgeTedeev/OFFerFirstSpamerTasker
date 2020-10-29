1411cfdffcf4cc626127917c30bea1696ec08ccd
George Tedeev <georgetd@mail.ru>
2020-10-29 19:09:10 +0300

    solved deserialization problem

--- src/main/java/com/project/offer/firstspamertasker/configs/KafkaConfig.java
+++ src/main/java/com/project/offer/firstspamertasker/configs/KafkaConfig.java
@@ -1,7 +1,5 @@
 package com.project.offer.firstspamertasker.configs;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.offer.firstspamertasker.deserializers.CustomDeserializer;
 import com.project.offer.firstspamertasker.entities.SpamTask;
 import com.project.offer.firstspamertasker.entities.User;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -12,7 +10,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -29,7 +26,7 @@ public class KafkaConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         return new DefaultKafkaConsumerFactory<>(props,new StringDeserializer(), deserializer);
     }
 
@@ -47,7 +44,7 @@ public class KafkaConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         return new DefaultKafkaConsumerFactory<>(props,new StringDeserializer(), deserializer);
     }
 
